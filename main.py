792dbea8a88b1197adbd18a81c233799e8934c65
Dunja Pavlovic <cydonia.oblonga.dunja@gmail.com>
2025-07-10 18:13:58 +0200

    Izmenjeni proizvodi i uklonjen prikaz iz main.py radi simulacije konflikta

--- main.py
+++ main.py
@@ -6,8 +6,3 @@ manager = ProductManager()
 manager.add_product(Product("Laptop", 1000, 5))
 manager.add_product(Product("Miš", 20, 10))
 manager.add_product(Product("Tastatura", 50, 7))
-
-print("Lista proizvoda:")
-manager.display_products()
-
-print(f"\nUkupna vrednost inventara: {manager.total_inventory_value()} dinara")
